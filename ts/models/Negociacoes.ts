334f6751ed53fd671139afbd07a6837ccb71105a
Ricardo Calixto <rcalixtos@gmail.com>
2019-10-14 10:28:12 -0300

    Criacao da classe de negociacoes que armazena a lista de negociacoes (array)

---
+++ ts/models/Negociacoes.ts
@@ -0,0 +1,11 @@
+class Negociacoes {
+  private negociacoes: Array<Negociacao> = [];
+
+  adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+
+  paraArray() {
+    return this.negociacoes;
+  }
+}
