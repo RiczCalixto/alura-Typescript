291a8006b8c6efed291b2f5d913aa246bcea2254
Ricardo Calixto <rcalixtos@gmail.com>
2019-10-14 22:39:21 -0300

    usando o system.js para lidar com os modulos

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -1,22 +1,42 @@
-import { MensagemView } from "./../views/MensagemView";
-import { NegociacoesView } from "./../views/NegociacoesView";
-import { Negociacoes } from "./../models/Negociacoes";
-import { Negociacao } from "./../models/Negociacao";
-export class NegociacaoController {
-    constructor() {
-        this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView("#negociacoesView");
-        this.mensagemView = new MensagemView("#mensagemView");
-        this.inputData = document.querySelector("#data");
-        this.inputQuantidade = (document.querySelector("#quantidade"));
-        this.inputValor = document.querySelector("#valor");
-        this.negociacoesView.update(this.negociacoes);
-    }
-    adiciona(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this.inputData.value.replace(/-/g, ",")), Number(this.inputQuantidade.value), Number(this.inputValor.value));
-        this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update("Negociação adicionada com sucesso!");
-    }
-}
+System.register(["./../views/MensagemView", "./../views/NegociacoesView", "./../models/Negociacoes", "./../models/Negociacao"], function (exports_1, context_1) {
+    "use strict";
+    var MensagemView_1, NegociacoesView_1, Negociacoes_1, Negociacao_1, NegociacaoController;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (MensagemView_1_1) {
+                MensagemView_1 = MensagemView_1_1;
+            },
+            function (NegociacoesView_1_1) {
+                NegociacoesView_1 = NegociacoesView_1_1;
+            },
+            function (Negociacoes_1_1) {
+                Negociacoes_1 = Negociacoes_1_1;
+            },
+            function (Negociacao_1_1) {
+                Negociacao_1 = Negociacao_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this.negociacoes = new Negociacoes_1.Negociacoes();
+                    this.negociacoesView = new NegociacoesView_1.NegociacoesView("#negociacoesView");
+                    this.mensagemView = new MensagemView_1.MensagemView("#mensagemView");
+                    this.inputData = document.querySelector("#data");
+                    this.inputQuantidade = (document.querySelector("#quantidade"));
+                    this.inputValor = document.querySelector("#valor");
+                    this.negociacoesView.update(this.negociacoes);
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    const negociacao = new Negociacao_1.Negociacao(new Date(this.inputData.value.replace(/-/g, ",")), Number(this.inputQuantidade.value), Number(this.inputValor.value));
+                    this.negociacoes.adiciona(negociacao);
+                    this.negociacoesView.update(this.negociacoes);
+                    this.mensagemView.update("Negociação adicionada com sucesso!");
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
--- js/views/NegociacoesView.js
+++ js/views/NegociacoesView.js
@@ -1,7 +1,17 @@
-import { View } from "./View";
-export class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var View_1, NegociacoesView;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
       <table class="table table-hover table-bordered">
         <thead>
             <tr>
@@ -14,22 +24,26 @@ export class NegociacoesView extends View {
   
         <tbody>
           ${model
-            .paraArray()
-            .map(negociacao => `
+                        .paraArray()
+                        .map(negociacao => `
               <tr>
                 <td>${negociacao.data.getDate()} / ${negociacao.data.getMonth() +
-            1} / ${negociacao.data.getFullYear()}</td>
+                        1} / ${negociacao.data.getFullYear()}</td>
                 <td>${negociacao.quantidade}</td>
                 <td>${negociacao.valor}</td>
                 <td>${negociacao.volume}</td>
               </tr>
             `)
-            .join("")}
+                        .join("")}
         </tbody>
   
         <tfoot>
         </tfoot>
       </table> 
       `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
